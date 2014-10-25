18e5464041a4d594b48770124e0bf0afb4c442c7
Jeremy <jleach@fcsinc.me>
2014-10-25 01:38:19 -0400

    more changes - fine tuning ajax functionality,

--- app/views/needs/create.blade.php
+++ app/views/needs/create.blade.php
@@ -8,12 +8,18 @@
 
 
 @section('content')
-{{ Form::open(array('action' => 'NeedController@postNeed', 'method' => 'post', 'id' => 'frm-add-need'))}}
-{{ Form::label('title', 'Need Title', array('id'=>'title','class' => 'form'))}}
-{{ Form::text('title')}}
-{{ Form::label('description', 'Description', array('id'=>'description','class' => 'form'))}}
-{{ Form::text('description')}}
-{{ Form::submit('Suiibmit the DAAADDTTAAA')}}
+{{ Form::open(array('action' => 'NeedController@postNeed', 'method' => 'post', 'id' => 'frm-add-need', 'class'=>'navbar-form navbar-left'))}}
+<div class='form-group'>
+    <h1><span class='label label-default'>Title
+{{ Form::text('title', '', array('class'=>'form-control', 'id' => 'title'))}}
+    </span></h1>
+    <h1> <span class= 'label label-default'>Description
+{{ Form::text('description', '',array('class'=>'form-control', 'id'=>'description'))}}
+        </span></h1>
+</div>
+<div>
+{{ Form::submit('Create Need', array('class'=>'form-control btn btn-default'))}}
+</div>
 {{ Form::close() }}
 
 @stop
@@ -29,12 +35,19 @@
         $( '#frm-add-need').on('submit', function(e){
             
             e.preventDefault();
+           
             var token = $('input[name=_token]').val();
             var title = $('#title').val();
             var description = $('#description').val();
             var action = "{{ URL::action('NeedController@postNeed')}}";
             var formData = 'title='+title+'&description='+description;
        // we should do saving animation herre
+            if(title==="")
+            {
+                alert("Enter title");
+                document.getElementById('title').focus();
+                return false;
+            }
             $.ajax({
                 type: "post",
                 url : action,
