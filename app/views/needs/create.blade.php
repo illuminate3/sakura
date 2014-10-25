0a598162a34409d84a03bc0af88f315bf87b4b0c
Jeremy <jleach@fcsinc.me>
2014-10-25 03:50:33 -0400

    more changes - adding progress meter for save function - it's got wings!

--- app/views/needs/create.blade.php
+++ app/views/needs/create.blade.php
@@ -10,15 +10,17 @@
 @section('content')
 {{ Form::open(array('action' => 'NeedController@postNeed', 'method' => 'post', 'id' => 'frm-add-need', 'class'=>'navbar-form navbar-left'))}}
 <div class='form-group'>
+    <span id='busy-icon'></span>
     <h1><span class='label label-default'>Title
-{{ Form::text('title', '', array('class'=>'form-control', 'id' => 'title'))}}
-    </span></h1>
-    <h1> <span class= 'label label-default'>Description
-{{ Form::text('description', '',array('class'=>'form-control', 'id'=>'description'))}}
+            {{ Form::text('title', '', array('class'=>'form-control', 'id' => 'title'))}}
         </span></h1>
+    <h1> <span class= 'label label-default'>Description
+            {{ Form::text('description', '',array('class'=>'form-control', 'id'=>'description'))}}
+        </span></h1>
+    
 </div>
 <div>
-{{ Form::submit('Create Need', array('class'=>'form-control btn btn-default'))}}
+    {{ Form::submit('Create Need', array('class'=>'form-control btn btn-default'))}}
 </div>
 {{ Form::close() }}
 
@@ -29,36 +31,43 @@
 
 @parent
 <script>
-    $("document").ready(function( $ ){
-      
-        
-        $( '#frm-add-need').on('submit', function(e){
-            
+    $("document").ready(function ($) {
+
+
+        $('#frm-add-need').on('submit', function (e) {
+
             e.preventDefault();
-           
+
             var token = $('input[name=_token]').val();
             var title = $('#title').val();
             var description = $('#description').val();
             var action = "{{ URL::action('NeedController@postNeed')}}";
-            var formData = 'title='+title+'&description='+description;
-       // we should do saving animation herre
-            if(title==="")
+            var formData = 'title=' + title + '&description=' + description;
+            // we should do saving animation herre id='busy-icon'
+            document.getElementById('busy-icon').innerHTML="<img src='../images/load-wings-small.gif'/>";
+            if (title === "")
             {
-                alert("Enter title");
                 document.getElementById('title').focus();
+                document.getElementById('busy-icon').innerHTML="";
+                return false;
+            } else
+            if (description === "") {
+                document.getElementById('description').focus();
+                document.getElementById('busy-icon').innerHTML="";
                 return false;
             }
             $.ajax({
                 type: "post",
-                url : action,
+                url: action,
                 data: formData,
-                success : function(data){
+                success: function (data) {
                     console.log(data);
                     $('#frm-add-need').trigger("reset");
+                    document.getElementById('busy-icon').innerHTML="Save Complete. Enter new Program or assign Needs";
                 }
             }, 'json');
             return false;
-        });   
+        });
     });
 </script>
 @stop
